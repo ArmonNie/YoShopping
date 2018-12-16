366fec68c6f2850da703461b832fbf428496d5d8
ArmonLoveLemon <2548079361@qq.com>
2018-12-16 21:19:24 +0800

    初次上传项目

---
+++ app/src/main/java/com/example/armon/yoshopping/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.armon.yoshopping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO: 2018/7/25 主页面功能列表
+    /*
+    准备：启动动画，自己画控件，创建文件的权限，调用相机权限，语言国际化等
+    后续：放到github，应用市场
+    版本问题，速度优化
+     */
+    /*
+    功能包括：1.网页浏览，关于浏览器内核
+    2，数据爬取（视频，图片等），视频播放，爬虫知识
+    3，短暂性图片保存，定期处理，图片加载以及显示
+    4.提供建议，功能扩展
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
